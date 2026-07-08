cada6127cd3d174568bed739162ee8e2b6cffd88
mai4211-netizen <maiiam4211@gmail.com>
2026-07-08 11:19:24 +0800

    Update IELTS writing template page

---
+++ ielts-writing-template/page-chunk-07.js
@@ -0,0 +1,2 @@
+window.__IELTS_PAGE_CHUNKS = window.__IELTS_PAGE_CHUNKS || [];
+window.__IELTS_PAGE_CHUNKS[7] = "ey==='Tab'&&!e.shiftKey){e.preventDefault();focusNextTextarea(t)}});\ndocument.querySelectorAll('textarea').forEach(autoGrowTextarea);\nscore();\n</script>\n</body>\n</html>\n";
